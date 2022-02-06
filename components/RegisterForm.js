ce1a428ce502e3013c989e44a0ac52ac167b341a
Dung Tran <pdung1989@gmail.com>
2022-02-06 11:04:35 +0200

    create structure, dummy data

---
+++ components/RegisterForm.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {StyleSheet, SafeAreaView, Text} from 'react-native';
+
+const RegisterForm = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text>This is for Login Form</Text>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingTop: 40,
+  },
+});
+
+export default RegisterForm;
