a556a1f663de0f9e1fd4d667bd71f32a58cce511
Dung Tran <pdung1989@gmail.com>
2022-02-07 14:58:14 +0200

    add MainProvider to App so that components can access the context

--- navigators/Navigator.js
+++ navigators/Navigator.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {NavigationContainer} from '@react-navigation/native';
 import Home from '../views/Home';
@@ -8,6 +8,8 @@ import AddPost from '../views/AddPost';
 import Favorites from '../views/Favorites';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import RecentPosts from '../views/RecentPosts';
+import {MainContext} from '../contexts/MainContext';
+import Login from '../views/Login';
 
 const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
@@ -25,18 +27,25 @@ const TabScreen = () => {
 };
 
 const StackScreen = () => {
+  const {isLoggedIn} = useContext(MainContext);
   return (
     <Stack.Navigator>
-      <Stack.Screen
-        name="MainPage"
-        component={TabScreen}
-        options={{headerShown: false}}
-      />
-      <Stack.Screen
-        name="RecentPosts"
-        component={RecentPosts}
-        options={{headerShown: false}}
-      ></Stack.Screen>
+      {isLoggedIn ? (
+        <>
+          <Stack.Screen
+            name="MainPage"
+            component={TabScreen}
+            options={{headerShown: false}}
+          />
+          <Stack.Screen
+            name="RecentPosts"
+            component={RecentPosts}
+            options={{headerShown: false}}
+          ></Stack.Screen>
+        </>
+      ) : (
+        <Stack.Screen name="Login" component={Login} />
+      )}
     </Stack.Navigator>
   );
 };
