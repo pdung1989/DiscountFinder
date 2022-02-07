da24c74812211ea70a521d9eea40674352560097
Dung Tran <pdung1989@gmail.com>
2022-02-07 15:05:14 +0200

    dummy login and logout

--- views/Profile.js
+++ views/Profile.js
@@ -1,12 +1,26 @@
-import React from 'react';
-import {StyleSheet, SafeAreaView, Text} from 'react-native';
+import React, {useContext} from 'react';
+import {StyleSheet, SafeAreaView, Text, Button} from 'react-native';
+import {MainContext} from '../contexts/MainContext';
+import PropTypes from 'prop-types';
 
-const Profile = () => {
+const Profile = ({navigation}) => {
+  const {setIsLoggedIn} = useContext(MainContext);
+
+  const logout = async () => {
+    setIsLoggedIn(false);
+    navigation.navigate('Login');
+  };
+
   return (
     <SafeAreaView style={styles.container}>
-      <Text>This is for Profile Page</Text>
+      <Text>Profile</Text>
+      <Button title={'Logout'} onPress={logout} />
     </SafeAreaView>
   );
+};
+
+Profile.propTypes = {
+  navigation: PropTypes.object,
 };
 
 const styles = StyleSheet.create({
