d311306a46101996ce1b44ab487a01ae76888fd1
Dung Tran <pdung1989@gmail.com>
2022-02-08 10:42:57 +0200

    fix importing components

--- views/Login.js
+++ views/Login.js
@@ -1,5 +1,5 @@
 import React, {useContext, useEffect} from 'react';
-import {StyleSheet, View, Text, Button} from 'react-native';
+import {StyleSheet, Text, TouchableOpacity, KeyboardAvoidingView} from 'react-native';
 import PropTypes from 'prop-types';
 import {MainContext} from '../contexts/MainContext';
 import AsyncStorage from '@react-native-async-storage/async-storage';
