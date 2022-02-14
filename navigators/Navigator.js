19d65407f7eb691b210634f23088194761d3ba8c
Dung Tran <pdung1989@gmail.com>
2022-02-14 15:55:09 +0200

    change color of active tab icon

--- navigators/Navigator.js
+++ navigators/Navigator.js
@@ -12,7 +12,7 @@ import {MainContext} from '../contexts/MainContext';
 import Login from '../views/Login';
 import Register from '../views/Register';
 import SuccessfulRegister from '../views/SuccessfulRegister';
-import { Ionicons } from "@expo/vector-icons";
+import {Ionicons} from '@expo/vector-icons';
 import EditProfile from '../views/EditProfile';
 
 const Tab = createBottomTabNavigator();
@@ -20,34 +20,47 @@ const Stack = createNativeStackNavigator();
 
 const TabScreen = () => {
   return (
-    <Tab.Navigator screenOptions={({route}) => ({
-      tabBarIcon: ({color}) => {
-        let iconName;
-        switch (route.name) {
-          case 'Home':
-            iconName = 'home-outline';
-            break;
-          case 'Browse':
-            iconName = 'menu-outline';
-            break;
-          case 'Add':
-            iconName = 'add-circle-outline';
-            break;
-          case 'Favorites':
-            iconName = 'heart-outline';
-            break;
-          case 'Profile':
-            iconName = 'person-circle-outline';
-            break;
-        }
-        return <Ionicons name={iconName} size={30} color={color} />;
-      },
-    })}>
-      <Tab.Screen name="Home" component={Home} options={{headerShown: false}}></Tab.Screen>
+    <Tab.Navigator
+      tabBarOptions={{
+        activeTintColor: '#467599',
+      }}
+      screenOptions={({route}) => ({
+        tabBarIcon: ({color}) => {
+          let iconName;
+          switch (route.name) {
+            case 'Home':
+              iconName = 'home-outline';
+              break;
+            case 'Browse':
+              iconName = 'menu-outline';
+              break;
+            case 'Add':
+              iconName = 'add-circle-outline';
+              break;
+            case 'Favorites':
+              iconName = 'heart-outline';
+              break;
+            case 'Profile':
+              iconName = 'person-circle-outline';
+              break;
+          }
+          return <Ionicons name={iconName} size={40} color={color} />;
+        },
+      })}
+    >
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{headerShown: false}}
+      ></Tab.Screen>
       <Tab.Screen name="Browse" component={Browse}></Tab.Screen>
       <Tab.Screen name="Add" component={Add}></Tab.Screen>
       <Tab.Screen name="Favorites" component={Favorites}></Tab.Screen>
-      <Tab.Screen name="Profile" component={Profile} options={{headerShown: false}}></Tab.Screen>
+      <Tab.Screen
+        name="Profile"
+        component={Profile}
+        options={{headerShown: false}}
+      ></Tab.Screen>
     </Tab.Navigator>
   );
 };
@@ -59,21 +72,39 @@ const StackScreen = () => {
     <Stack.Navigator>
       {isLoggedIn ? (
         <>
-          <Stack.Screen name="MainPage" component={TabScreen} options={{headerShown: false}}
+          <Stack.Screen
+            name="MainPage"
+            component={TabScreen}
+            options={{headerShown: false}}
           />
           {/* <Stack.Screen
             name="List"
             component={TabScreen}
             options={{headerTransparent: true, headerShown: false}}
           ></Stack.Screen> */}
-          <Stack.Screen name="Single" component={Single} options={{headerShown: false}}
-         />
-         <Stack.Screen name="Edit profile" component={EditProfile} options={{headerShown: false}} />
+          <Stack.Screen
+            name="Single"
+            component={Single}
+            options={{headerShown: false}}
+          />
+          <Stack.Screen
+            name="Edit profile"
+            component={EditProfile}
+            options={{headerShown: false}}
+          />
         </>
       ) : (
         <>
-          <Stack.Screen name="Login" component={Login} options={{headerShown: false}}/>
-          <Stack.Screen name="Register" component={Register} options={{headerShown: false}}/>
+          <Stack.Screen
+            name="Login"
+            component={Login}
+            options={{headerShown: false}}
+          />
+          <Stack.Screen
+            name="Register"
+            component={Register}
+            options={{headerShown: false}}
+          />
           <Stack.Screen
             name="SuccessfulRegister"
             component={SuccessfulRegister}
