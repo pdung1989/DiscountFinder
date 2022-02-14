cded3de1121ad4909fc1e4d700958338b8144424
Dung Tran <pdung1989@gmail.com>
2022-02-14 16:12:59 +0200

    separate edit profile info and password

--- views/EditProfile.js
+++ views/EditProfile.js
@@ -28,6 +28,7 @@ const EditProfile = ({navigation}) => {
     mode: 'onBlur',
   });
 
+  // edit profile information
   const onSubmit = async (data) => {
     console.log(data);
     try {
@@ -48,6 +49,26 @@ const EditProfile = ({navigation}) => {
     }
   };
 
+  // change password
+  const onSubmitPassword = async (data) => {
+    console.log(data);
+    try {
+      delete data.confirmPassword;
+      if (data.password === '') {
+        delete data.password;
+      }
+      const userToken = await AsyncStorage.getItem('userToken');
+      const userData = await putUser(data, userToken);
+      if (userData) {
+        Alert.alert('Success', userData.message);
+        delete data.password;
+        setUser(data);
+      }
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+
   return (
     <SafeAreaView>
       <View style={styles.title}>
@@ -59,140 +80,144 @@ const EditProfile = ({navigation}) => {
         />
         <Title>Profile</Title>
       </View>
-      <Controller
-        control={control}
-        rules={{
-          required: {value: true, message: 'This is required.'},
-          minLength: {
-            value: 3,
-            message: 'Username has to be at least 3 characters.',
-          },
-          validate: async (value) => {
-            try {
-              const available = await checkUsername(value);
-              if (available || user.username === value) {
-                return true;
-              } else {
-                return 'Username is already taken.';
+      <View>
+        <Controller
+          control={control}
+          rules={{
+            required: {value: true, message: 'This is required.'},
+            minLength: {
+              value: 3,
+              message: 'Username has to be at least 3 characters.',
+            },
+            validate: async (value) => {
+              try {
+                const available = await checkUsername(value);
+                if (available || user.username === value) {
+                  return true;
+                } else {
+                  return 'Username is already taken.';
+                }
+              } catch (error) {
+                throw new Error(error.message);
               }
-            } catch (error) {
-              throw new Error(error.message);
-            }
-          },
-        }}
-        render={({field: {onChange, onBlur, value}}) => (
-          <Input
-            onBlur={onBlur}
-            onChangeText={onChange}
-            value={value}
-            autoCapitalize="none"
-            placeholder="Username"
-            errorMessage={errors.username && errors.username.message}
-          />
-        )}
-        name="username"
-      />
-
-      <Controller
-        control={control}
-        rules={{
-          minLength: {
-            value: 5,
-            message: 'Password has to be at least 5 characters.',
-          },
-          /*
+            },
+          }}
+          render={({field: {onChange, onBlur, value}}) => (
+            <Input
+              onBlur={onBlur}
+              onChangeText={onChange}
+              value={value}
+              autoCapitalize="none"
+              placeholder="Username"
+              errorMessage={errors.username && errors.username.message}
+            />
+          )}
+          name="username"
+        />
+        <Controller
+          control={control}
+          rules={{
+            required: {value: true, message: 'This is required.'},
+            pattern: {
+              value: /\S+@\S+\.\S+$/,
+              message: 'Has to be valid email.',
+            },
+          }}
+          render={({field: {onChange, onBlur, value}}) => (
+            <Input
+              onBlur={onBlur}
+              onChangeText={onChange}
+              value={value}
+              autoCapitalize="none"
+              placeholder="Email"
+              errorMessage={errors.email && errors.email.message}
+            />
+          )}
+          name="email"
+        />
+        <Controller
+          control={control}
+          rules={{
+            minLength: {
+              value: 3,
+              message: 'Full name has to be at least 3 characters.',
+            },
+          }}
+          render={({field: {onChange, onBlur, value}}) => (
+            <Input
+              onBlur={onBlur}
+              onChangeText={onChange}
+              value={value}
+              autoCapitalize="words"
+              placeholder="Full name"
+              errorMessage={errors.full_name && errors.full_name.message}
+            />
+          )}
+          name="full_name"
+        />
+        <Button title="Submit" onPress={handleSubmit(onSubmit)} />
+      </View>
+      <View>
+        <Controller
+          control={control}
+          rules={{
+            minLength: {
+              value: 5,
+              message: 'Password has to be at least 5 characters.',
+            },
+            /*
           pattern: {
             value: /(?=.*[\p{Lu}])(?=.*[0-9]).{8,}/u,
             message: 'Min 8, Uppercase, Number',
           },
           */
-        }}
-        render={({field: {onChange, onBlur, value}}) => (
-          <Input
-            onBlur={onBlur}
-            onChangeText={onChange}
-            value={value}
-            autoCapitalize="none"
-            secureTextEntry={true}
-            placeholder="Password"
-            errorMessage={errors.password && errors.password.message}
-          />
-        )}
-        name="password"
-      />
+          }}
+          render={({field: {onChange, onBlur, value}}) => (
+            <Input
+              onBlur={onBlur}
+              onChangeText={onChange}
+              value={value}
+              autoCapitalize="none"
+              secureTextEntry={true}
+              placeholder="Password"
+              errorMessage={errors.password && errors.password.message}
+            />
+          )}
+          name="password"
+        />
 
-      <Controller
-        control={control}
-        rules={{
-          validate: (value) => {
-            const {password} = getValues();
-            if (value === password) {
-              return true;
-            } else {
-              return 'Passwords do not match.';
-            }
-          },
-        }}
-        render={({field: {onChange, onBlur, value}}) => (
-          <Input
-            onBlur={onBlur}
-            onChangeText={onChange}
-            value={value}
-            autoCapitalize="none"
-            secureTextEntry={true}
-            placeholder="Confirm Password"
-            errorMessage={
-              errors.confirmPassword && errors.confirmPassword.message
-            }
-          />
-        )}
-        name="confirmPassword"
-      />
-
-      <Controller
-        control={control}
-        rules={{
-          required: {value: true, message: 'This is required.'},
-          pattern: {
-            value: /\S+@\S+\.\S+$/,
-            message: 'Has to be valid email.',
-          },
-        }}
-        render={({field: {onChange, onBlur, value}}) => (
-          <Input
-            onBlur={onBlur}
-            onChangeText={onChange}
-            value={value}
-            autoCapitalize="none"
-            placeholder="Email"
-            errorMessage={errors.email && errors.email.message}
-          />
-        )}
-        name="email"
-      />
-
-      <Controller
-        control={control}
-        rules={{
-          minLength: {
-            value: 3,
-            message: 'Full name has to be at least 3 characters.',
-          },
-        }}
-        render={({field: {onChange, onBlur, value}}) => (
-          <Input
-            onBlur={onBlur}
-            onChangeText={onChange}
-            value={value}
-            autoCapitalize="words"
-            placeholder="Full name"
-            errorMessage={errors.full_name && errors.full_name.message}
-          />
-        )}
-        name="full_name"
-      />
-
-      <Button title="Submit" onPress={handleSubmit(onSubmit)} />
+        <Controller
+          control={control}
+          rules={{
+            validate: (value) => {
+              const {password} = getValues();
+              if (value === password) {
+                return true;
+              } else {
+                return 'Passwords do not match.';
+              }
+            },
+          }}
+          render={({field: {onChange, onBlur, value}}) => (
+            <Input
+              onBlur={onBlur}
+              onChangeText={onChange}
+              value={value}
+              autoCapitalize="none"
+              secureTextEntry={true}
+              placeholder="Confirm Password"
+              errorMessage={
+                errors.confirmPassword && errors.confirmPassword.message
+              }
+            />
+          )}
+          name="confirmPassword"
+        />
+        <Button
+          title="Change password"
+          onPress={handleSubmit(onSubmitPassword)}
+        />
+      </View>
     </SafeAreaView>
   );
 };
